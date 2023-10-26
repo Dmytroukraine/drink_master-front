0016c28064ec35a03486371d3d2c09cdaae897cd
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-10-26 06:36:49 +0300

    style + redux

---
+++ src/redux/userSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  _id: '',
+  name: '',
+  email: '',
+  avatarURL: '',
+  token: null,
+  isLoggedIn: false,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: {
+      reducer(state, action) {
+        state = { ...state, ...action.payload.value, isLoggedIn: true };
+        return state;
+      },
+      prepare(value) {
+        return {
+          payload: { value },
+        };
+      },
+    },
+    resetUser: {
+      reducer(state) {
+        state = initialState;
+        return state;
+      },
+    },
+  },
+});
+
+export const { setUser, resetUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
