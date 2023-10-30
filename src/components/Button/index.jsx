4c5442fe9cd5cffa5b0ffc27cd5b0543d64afb1a
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2023-10-30 02:59:53 +0200

    added media query + props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,32 @@
 import s from './button.module.css';
 
+// props:
+// text: text - button
 // buttonClass: default = button, submitButton, addButton
+// width: '250px'
+// padding: '10px 15px 10px 30px'
+// fontSize: '14px'
 
-export const Button = ({ text, buttonClass = 'button' }) => {
-  return <button className={s[buttonClass]}>{text}</button>;
+//Example:
+// <Button text="Click here!" padding='0 0 0 0 0' width='250px' fontSize='25px'/>
+
+export const Button = ({
+  text,
+  buttonClass = 'button',
+  width = '',
+  padding = '',
+  fontSize= ''
+}) => {
+  return (
+    <button
+      style={{
+        width: `${width}`,
+        padding: `${padding}`,
+        fontSize: `${fontSize}`
+      }}
+      className={s[buttonClass]}
+    >
+      {text}
+    </button>
+  );
 };
