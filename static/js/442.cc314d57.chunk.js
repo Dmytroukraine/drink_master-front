e1f6a50de7c0be84e0dcb645133897c20c0ea937
Dmytroukraine <120205405+Dmytroukraine@users.noreply.github.com>
2023-11-02 22:27:18 +0000

    Deploying to gh-pages from @ Dmytroukraine/drink_master-front@52da9f38f8d2db24a7c391db74541cb6bbf58ce1 🚀

---
+++ static/js/442.cc314d57.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdrink_master_front=self.webpackChunkdrink_master_front||[]).push([[442],{6442:function(n,r,e){e.r(r),e.d(r,{default:function(){return i}});var t="Error_text__2LC0m",s=e(184),u=function(){return(0,s.jsxs)("p",{className:t,children:[(0,s.jsx)("span",{children:"4"}),"4"]})},c=e(892),i=function(){return(0,s.jsx)(c.Z,{children:(0,s.jsx)(u,{})})}}}]);
+//# sourceMappingURL=442.cc314d57.chunk.js.map
