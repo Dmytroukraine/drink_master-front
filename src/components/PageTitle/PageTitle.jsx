04af9c631b97bdb87e5e74241988ed8c07b26a1c
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-11-02 20:16:41 +0200

    Update PageTitle.jsx

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -3,7 +3,7 @@ import css from './PageTitle.module.css';
 export const PageTitle = ({ title }) => {
   return (
     <div className={css.TitleWrap}>
-      <h1 className={css.PageTitle}>{title}</h1>;
+      <h1 className={css.PageTitle}>{title}</h1>
     </div>
   );
 };
