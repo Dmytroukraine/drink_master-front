85759b06497b020c3d2ee596de532b06dac1a97a
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2023-11-04 15:06:52 +0200

    add logic render cocktails cart
    Render cards on dependensive width screen

--- src/components/HomePage/CocktailList.jsx
+++ src/components/HomePage/CocktailList.jsx
@@ -2,44 +2,83 @@ import { useEffect, useState } from 'react';
 import s from './PreviewDrinks.module.css';
 import { CocktailListItem } from './CocktailListItem';
 
-export const CocktailList = ({ data, title }) => {
-  const [visibleCacktails, setVisibleCacktails] = useState(null);
+const debounce = (func, wait, immediate) => {
+  var timeout;
+  return () => {
+    const context = this,
+      args = null;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+export const CocktailList = ({ category, title }) => {
+  const [widthScreen, setWidthScreen] = useState(window.screen.width);
+  const [visibleCocktails, setVisibleCocktails] = useState(null);
 
   const mediaSmall = window.matchMedia('(max-width: 767px)').matches;
   const mediaMedium = window.matchMedia('(max-width: 1439px)').matches;
-  // const mediaLarge = window.matchMedia('(min-width: 1440px)').matches;
 
-  const listenerScreen = window.screen.width;
-
-  const sliceData = data => {
-    const mobile = data?.slice(0, 1);
-    const tab = data?.slice(0, 2);
-    // const desk = data;
+  const sliceCocktails = category => {
+    const mobile = category?.slice(0, 1);
+    const tab = category?.slice(0, 2);
 
     if (mediaSmall) return mobile;
     if (mediaMedium) return tab;
-    // if (mediaLarge) return desk;
-
-    return data;
+    return category;
   };
 
   useEffect(() => {
-    const mql = window.matchMedia(`(width: ${listenerScreen})`);
-    mql.addEventListener('change', sliceData);
-    setVisibleCacktails(sliceData(data));
+    setVisibleCocktails(sliceCocktails(category));
+  }, [widthScreen]);
+
+  const resizeObserver = new ResizeObserver(entries => {
+    for (const entry of entries) {
+      const contentBoxSize = entry.contentBoxSize[0];
+
+      if (contentBoxSize.inlineSize < 767) {
+        return setWidthScreen(window.screen.width);
+      }
+      if (contentBoxSize.inlineSize > 767 && contentBoxSize.inlineSize < 1439) {
+        return setWidthScreen(window.screen.width);
+      }
+      return setWidthScreen(window.screen.width);
+    }
+  });
+
+  useEffect(() => {
+    window.addEventListener(
+      'resize',
+      debounce(() => resizeObserver.observe(document.body), 3000, false),
+      false
+    );
+
     return () => {
-      mql.removeEventListener('change', sliceData);
+      window.removeEventListener(
+        'resize',
+        debounce(() => resizeObserver.observe(document.body), 200, false),
+        false
+      );
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+
+
+  // console.log('Check category: ', visibleCocktails.length )
 
   return (
     <>
-      {visibleCacktails ? (
+      {visibleCocktails ? (
         <>
           <h3 className={s.categoryTitle}>{title}</h3>
           <ul className={s.list}>
-            {visibleCacktails?.map(({ _id, drink, drinkThumb }) => {
+            {visibleCocktails?.map(({ _id, drink, drinkThumb }) => {
               return (
                 <CocktailListItem
                   key={_id}
--- src/components/HomePage/CocktailListItem.jsx
+++ src/components/HomePage/CocktailListItem.jsx
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import s from './PreviewDrinks.module.css';
-// import defaultCocktailPoster from '../../images/thumb-placeholder-large.png';
+import defaultCocktailPoster from '../../images/thumb-placeholder-large.png';
 
 export const CocktailListItem = ({ pictureURL, title, id }) => {
-
   return (
     <li className={s.card}>
       <div className={s.thumb}>
-        <img src={pictureURL} alt={`photo ${title} cocktail`}></img>
+        <img
+          src={pictureURL}
+          alt={`${title} cocktail`}
+          onError={e => (e.target.src = defaultCocktailPoster)}
+        ></img>
       </div>
       <div className={s.footerCard}>
         <p className={s.name}>{title}</p>
