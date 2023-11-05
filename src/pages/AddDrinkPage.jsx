3c0d79ea5cc83b5a46c87b623597388703be4504
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-11-05 15:00:44 +0200

    add adaptive

--- src/pages/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage.jsx
@@ -1,11 +1,9 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
-import { PopularRecipe } from 'components/AddRecipePage/PopularRecipe';
 
 const AddDrinkPage = () => {
   return (
     <div>
       <PageTitle title={'Add drink'} />
-      <PopularRecipe/>
     </div>
   );
 };
--- src/redux/drinkSlice/drinksSlice.js
+++ src/redux/drinkSlice/drinksSlice.js
@@ -34,6 +34,6 @@ export const drinksApi = createApi({
 });
 
 export const {
-  usegetDrinkMainPageQuery,
+  useGetDrinkMainPageQuery,
   useGetDrinkByIdQuery,
 } = drinksApi;
