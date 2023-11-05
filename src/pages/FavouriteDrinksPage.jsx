ccd0e3bdc05e307189717c8adaf3e96a96b350b8
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-11-05 22:04:55 +0200

    add_pag_no_style

--- src/pages/FavouriteDrinksPage.jsx
+++ src/pages/FavouriteDrinksPage.jsx
@@ -1,46 +1,69 @@
-
 import { Loading } from '../components/Loader/Loader';
 import { useGetDrinkFavoriteAllQuery } from 'redux/drinkSlice/drinkFavoriteSlice';
 import { PageTitle } from '../components/PageTitle/PageTitle';
-// import { Paginator } from '../components/Paginator/Paginator';
+import { Paginator } from '../components/Paginator/Paginator';
 import NoContentSection from 'components/404Page/NoContent';
 import { DrinksList } from '../components/FavouriteDrinksPage/DrinkList';
 import BasicImg from '../images/blue_iced_tea_large@1x.png';
-
+import { useState } from 'react';
 
 import css from './FavouriteDrinksPage.module.css';
 
 const FavouriteDrinksPage = () => {
   const { data = [], isLoading } = useGetDrinkFavoriteAllQuery();
-  // console.log(useGetDrinkFavoriteAllQuery());
-  // console.log(data);
-  // console.log(data.length);
+  const [pagData, setPagData] = useState(data);
 
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const quantityDrinks = data.length;
+
+  const itemsPerPage = 3;
+  const quantityPages = Math.ceil(quantityDrinks / itemsPerPage);
+
+  const i = 0 + currentPage * itemsPerPage;
+  const k = itemsPerPage + itemsPerPage * currentPage;
+
+  const pagDrinks = data.slice(i, k);
+
+  const setPage = page => {
+    setCurrentPage(page);
+    setPagData(pagData);
+  };
 
   return (
     <div>
       {/* {isError && { isError }} */}
       <PageTitle title="Favorites" />
       <div>
-
         {isLoading && <Loading />}
-        {data.length > 0 ? (<DrinksList drinks={data} />) : (<NoContentSection children={
-          <div>
-          <img
-            className={css.drinkNotImg}
-            src={BasicImg}
-            alt="A coctail"
-            width="261"
-            height="326"
+        {data.length > 0 ? (
+          <DrinksList drinks={pagDrinks} />
+        ) : (
+          <NoContentSection
+            children={
+              <div>
+                <img
+                  className={css.drinkNotImg}
+                  src={BasicImg}
+                  alt="A coctail"
+                  width="261"
+                  height="326"
+                />
+                <p className={css.drinkNotImgText}>
+                  You haven't added any favorite cocktails yet
+                </p>
+              </div>
+            }
           />
-          <p className={css.drinkNotImgText}>
-            You haven't added any favorite cocktails yet
-          </p>
-        </div>
-        }/>)
-        }
-        {/* {data.length > 1 && <Paginator drinks={data} />} */}
-
+        )}
+        {data.length > 1 && (
+          <Paginator
+            drinks={data}
+            quantityPages={quantityPages}
+            setPage={setPage}
+            currentPage={currentPage}
+          />
+        )}
       </div>
     </div>
   );
