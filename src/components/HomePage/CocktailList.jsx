b23b6c9a51c0d8fd2a1d753dee2af871d660084c
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2023-11-05 16:09:35 +0200

    add check for

--- src/components/HomePage/CocktailList.jsx
+++ src/components/HomePage/CocktailList.jsx
@@ -20,33 +20,46 @@ const debounce = (func, wait, immediate) => {
 
 export const CocktailList = ({ category, title }) => {
   const [widthScreen, setWidthScreen] = useState(window.screen.width);
-  const [visibleCocktails, setVisibleCocktails] = useState(null);
+  const [visibleCocktails, setVisibleCocktails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const mediaSmall = window.matchMedia('(max-width: 767px)').matches;
   const mediaMedium = window.matchMedia('(max-width: 1439px)').matches;
 
-  const sliceCocktails = useCallback((category) => {
-    const mobile = category?.slice(0, 1);
-    const tab = category?.slice(0, 2);
+  const sliceCocktails = useCallback(
+    category => {
+      const mobile = category?.slice(0, 1);
+      const tab = category?.slice(0, 2);
 
-    if (mediaSmall) return mobile;
-    if (mediaMedium) return tab;
-    return category;
-  }, [mediaSmall, mediaMedium]);
+      if (mediaSmall) return mobile;
+      if (mediaMedium) return tab;
+      return category;
+    },
+    [mediaSmall, mediaMedium]
+  );
 
   useEffect(() => {
+    setIsLoading(true);
     setVisibleCocktails(sliceCocktails(category));
+    setIsLoading(false);
+
+    console.log('visibleCocktail: ', visibleCocktails);
   }, [category, sliceCocktails]);
 
+  console.log('visibleCocktail: ', visibleCocktails);
+
   const resizeObserver = useMemo(() => {
-    return new ResizeObserver((entries) => {
+    return new ResizeObserver(entries => {
       for (const entry of entries) {
         const contentBoxSize = entry.contentBoxSize[0];
 
         if (contentBoxSize.inlineSize < 767) {
           setWidthScreen(window.screen.width);
         }
-        if (contentBoxSize.inlineSize > 767 && contentBoxSize.inlineSize < 1439) {
+        if (
+          contentBoxSize.inlineSize > 767 &&
+          contentBoxSize.inlineSize < 1439
+        ) {
           setWidthScreen(window.screen.width);
         }
         setWidthScreen(window.screen.width);
@@ -73,22 +86,24 @@ export const CocktailList = ({ category, title }) => {
 
   return (
     <>
-      {visibleCocktails ? (
-        <>
-          <h3 className={s.categoryTitle}>{title}</h3>
-          <ul className={s.list}>
-            {visibleCocktails?.map(({ _id, drink, drinkThumb }) => {
-              return (
-                <CocktailListItem
-                  key={_id}
-                  pictureURL={drinkThumb}
-                  title={drink}
-                  id={_id}
-                ></CocktailListItem>
-              );
-            })}
-          </ul>
-        </>
+      {!isLoading ? (
+        visibleCocktails.length !== 0 ? (
+          <>
+            <h3 className={s.categoryTitle}>{title}</h3>
+            <ul className={s.list}>
+              {visibleCocktails?.map(({ _id, drink, drinkThumb }) => {
+                return (
+                  <CocktailListItem
+                    key={_id}
+                    pictureURL={drinkThumb}
+                    title={drink}
+                    id={_id}
+                  ></CocktailListItem>
+                );
+              })}
+            </ul>
+          </>
+        ) : null
       ) : null}
     </>
   );
