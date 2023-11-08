9e7421c70ebe3720ae33a5c8a78d091a84bee2b7
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-11-08 13:35:11 +0200

    Loader return

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,28 +1,18 @@
-// import { Circles } from 'react-loader-spinner';
-// import css from './Loader.module.css';
-
-// export const Loading = () => {
-//   return (
-//     <div className={css.LoaderWrap}>
-//       <Circles
-//         height="80"
-//         width="80"
-//         color="#659fd1"
-//         ariaLabel="circles-loading"
-//         wrapperStyle={{}}
-//         wrapperClass=""
-//         visible={true}
-//       />
-//     </div>
-//   );
-// };
-
+import { Circles } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loading = () => {
   return (
     <div className={css.LoaderWrap}>
-      
+      <Circles
+        height="80"
+        width="80"
+        color="#659fd1"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 };
